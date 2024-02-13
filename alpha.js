d6bc169f862f91652a849d475c08fce3c1ae52e9
atique-shahriar <you@example.comsatique06@gmail.com>
2024-02-13 23:49:34 +0600

    Alphabet Showing in Display Added

--- alpha.js
+++ alpha.js
@@ -6,6 +6,23 @@ function homePlayBtnFunction() {
 
   const playGroundPage = document.getElementById("play-ground");
   playGroundPage.classList.remove("hidden");
+  continueGame();
 
+}
 
+function continueGame() {
+  const randomAlphabet = getARandomAlphabet();
+  console.log(randomAlphabet);
+
+  const alphabetLetter = document.getElementById('alphabetDisplay');
+  alphabetLetter.innerText = randomAlphabet;
+  const keyBtn=document.getElementById(randomAlphabet);
+  keyBtn.classList.add("bg-orange-500")
+}
+
+function getARandomAlphabet() {
+  const alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const randomIndex = Math.round(Math.random() * 25);
+  const alphabet = alphabets[randomIndex];
+  return alphabet;
 }
