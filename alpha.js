763ac321d44fbd42ece7a61cebebfc4f403f1ed4
atique-shahriar <you@example.comsatique06@gmail.com>
2024-02-14 20:06:40 +0600

    Score and Life functionality added

--- alpha.js
+++ alpha.js
@@ -16,8 +16,9 @@ function continueGame() {
 
   const alphabetLetter = document.getElementById('alphabetDisplay');
   alphabetLetter.innerText = randomAlphabet;
-  const keyBtn=document.getElementById(randomAlphabet);
+  const keyBtn = document.getElementById(randomAlphabet);
   keyBtn.classList.add("bg-orange-500");
+
 }
 
 function getARandomAlphabet() {
@@ -26,3 +27,34 @@ function getARandomAlphabet() {
   const alphabet = alphabets[randomIndex];
   return alphabet;
 }
+
+
+function handleKeyboardButtonPress(event) {
+  const playerPressed = event.key;
+  const displayAlpha = document.getElementById("alphabetDisplay").innerText;
+  if (playerPressed === displayAlpha) {
+    console.log(true);
+    const keyBtn = document.getElementById(playerPressed);
+    keyBtn.classList.remove("bg-orange-500");
+    const score = document.getElementById("scoreBtn");
+    const scoreVal = parseInt(score.innerText);
+    score.innerText = scoreVal + 1;
+    continueGame();
+  }
+  else {
+    console.log(false);
+    const life = document.getElementById("lifeBtn");
+    const lifeValue = parseInt(life.innerText);
+
+    if (lifeValue === 0) {
+      const playGroundPage = document.getElementById("play-ground");
+      playGroundPage.classList.add("hidden");
+      const scorePage = document.getElementById("score");
+      scorePage.classList.remove("hidden");
+    }
+    life.innerText = lifeValue - 1;
+
+  }
+}
+document.addEventListener("keyup", handleKeyboardButtonPress)
+
